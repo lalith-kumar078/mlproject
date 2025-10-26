d223a872084c3067e300c27518f5a17cb197a059
lalith <lalithvkr19@gmail.com>
2025-10-26 16:35:33 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,8 +49,35 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                },
+                "K-Neighbors Regressor": {  
+                    'n_neighbors': [3, 5, 7, 9],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['ball_tree', 'kd_tree', 'brute']
+                },
+                "Linear Regression": {}, 
+                "XGBRegressor": {
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'n_estimators': [50, 100, 200]
+                },
+                "CatBoost Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
 
-            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models)
+            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params)
 
             #to get best model score from dict
             best_model_score = max(sorted(model_report.values()))
